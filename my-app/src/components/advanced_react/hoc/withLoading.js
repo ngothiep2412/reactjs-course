a069f5703f342d6c392a13340582cf828a1b1b18
NgoThiep2412 <ngothiep2412@gmail.com>
2022-06-30 21:15:18 +0700

    update recap hocs

---
+++ my-app/src/components/advanced_react/hoc/withLoading.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function withLoading(Component, url) {
+  return (props) => {
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        setNews(data.hits);
+      }
+      fetchData();
+    }, []);
+    if (!news || news.length === 0) return <div>Loading ...</div>;
+    return <Component data={news} {...props}></Component>;
+  };
+}
+
+export default withLoading;
+// High order function: map, filter, some, every, reduce
+
+// [1,2,3].map((item, array) => {})
