d52f6362fca725ae2ca74a3c738b97db589cc2a3
NgoThiep2412 <ngothiep2412@gmail.com>
2022-07-02 14:48:07 +0700

    update prop getters

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,26 +3,38 @@ import Switch from "./components/swtich/Switch";
 function useToggle() {
   const [on, setOn] = useState(false);
   const toggle = () => setOn(!on);
+  function getToggleProps({ onClick, ...rest } = {}) {
+    return {
+      onClick: () => {
+        onClick && onClick();
+        toggle();
+      },
+      ...rest,
+    };
+  }
   // toggleProps
   return {
     on,
     toggle,
-    toggleProps: {
-      onClick: toggle,
-    },
+    getToggleProps,
+    // toggleProps: {
+    //   onClick: toggle,
+    // },
   };
 }
 // Props collection  - Kentc Dodds - creator of Remix
 // Props getter
 function App() {
-  const { on, toggleProps } = useToggle();
+  const { on, toggleProps, getToggleProps } = useToggle();
   return (
     <div>
-      <Switch on={on} {...toggleProps}></Switch>
+      <Switch {...getToggleProps({ on })}></Switch>
       <hr />
       <button
         aria-label="custom-button"
-        {...toggleProps}
+        {...getToggleProps({
+          onClick: () => console.info("onButtonClick"),
+        })}
         // onClick={() => {
         //   console.log("toggle");
         //   toggle();
